1813efdcb11d47e020be004060e8bc56e0ef7fdb
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2021-02-20 13:13:23 +0700

    [ADD] index.html and main.js

---
+++ 62130500121_groupwork_4/main.js
@@ -0,0 +1,48 @@
+const app ={
+    data(){
+        return{
+            pictures:[
+                {path: './images/irene2.jpg', member:'Irene', heart: false, showModal: false},
+                {path: './images/seulgi2.jpg', member:'Seulgi', heart: false, showModal: false},
+                {path: './images/wendy2.jpg', member:'Wendy', heart: false, showModal: false},
+                {path: './images/joy2.jpg', member:'Joy', heart: false, showModal: false},
+                {path: './images/yeri2.jpg', member:'Yeri', heart: false, showModal: false}
+            ],
+            searchPic:'',
+            toggleSearch: true,
+            
+        }
+    },
+    methods:{
+        like(text){
+            const index = this.pictures.findIndex(p => p.member == text)
+            this.pictures[index].heart = !this.pictures[index].heart
+        },
+        startSearch(){
+            this.toggleSearch =! this.toggleSearch
+        },
+        cancelSearch(){
+            this.toggleSearch =! this.toggleSearch
+            this.searchPic = ''
+        },
+        toggleModel(text){
+            const index = this.pictures.findIndex(p => p.member == text)
+            this.pictures[index].showModal = !this.pictures[index].showModal;
+        }
+    },
+    computed:{
+        countPic(){
+            return this.pictures.length
+        },
+        filteredPhoto(){
+            return this.pictures.filter(pictures => {
+                return pictures.member.toLowerCase().includes(this.searchPic.toLowerCase())
+            })
+        },
+        countLike() {
+            return this.pictures.filter(p => p.heart).length;
+        },
+    }
+}
+
+Vue.createApp(app).mount('#app')
